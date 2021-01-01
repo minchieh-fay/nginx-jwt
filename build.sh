80f7a4f80f9f16de13f13d50e3ab2c8cb6ac1ea6
minchieh-fay <minchieh@hotmail.com>
2021-01-02 02:17:30 +0800

    324234

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --platform=linux/amd64,linux/arm64 -t minchieh/ngx-jwt . --push
+docker buildx build --platform=linux/amd64,linux/arm64 -t minchieh/nginx-jwt . --push
